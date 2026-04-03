e43cf3e46a118f04ebff69dc5054924c11ec2838
Sidhant-1299 <sidhant.raj.khati@gmail.com>
2026-04-02 22:08:44 -0400

    updated db init for psql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -16,13 +16,12 @@ WARNING:
 -- USE master;
 -- GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+-- Drop and recreate the 'DataWarehouse' database PSQL
+IF EXISTS (SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'DataWarehouse')
 BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE DataWarehouse;
 END;
-GO
 
 -- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
