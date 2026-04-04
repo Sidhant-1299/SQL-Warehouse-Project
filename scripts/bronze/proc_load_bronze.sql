e4c0986871f9bbf748f681f3d9b2711c16503bcd
Sidhant-1299 <sidhant.raj.khati@gmail.com>
2026-04-04 01:18:48 -0400

    Truncat added to bronze layer data to prevent multiple data copy

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -27,20 +27,26 @@ Notes:
 
 
 -- Load in to bronze.crm_cust_info
+TRUNCATE TABLE bronze.crm_cust_info;
 \copy bronze.crm_cust_info FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_crm/cust_info.csv' WITH (FORMAT csv, HEADER true)
 
 -- Load in to bronze.crm_prd_info
+TRUNCATE TABLE bronze.crm_prd_info;
 \copy bronze.crm_prd_info FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_crm/prd_info.csv' WITH (FORMAT csv, HEADER true)
 
 -- Load in to bronze.crm_sales_details
+TRUNCATE TABLE bronze.crm_sales_details;
 \copy bronze.crm_sales_details FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_crm/sales_details.csv' WITH (FORMAT csv, HEADER true)
 
 -- Load in to bronze.erp_cust_az12 from file
+TRUNCATE TABLE bronze.erp_cust_az12;
 \copy bronze.erp_cust_az12 FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_erp/cust_az12.csv' WITH (FORMAT csv, HEADER true)
 
 -- Load in to bronze.erp_loc_a101 from file
+TRUNCATE TABLE bronze.erp_loc_a101;
 \copy bronze.erp_loc_a101 FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_erp/loc_a101.csv' WITH (FORMAT csv, HEADER true)
 
 
 -- Load in to bronze.erp_px_cat_g1v2 from file
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 \copy bronze.erp_px_cat_g1v2 FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_erp/px_cat_g1v2.csv' WITH (FORMAT csv, HEADER true)
