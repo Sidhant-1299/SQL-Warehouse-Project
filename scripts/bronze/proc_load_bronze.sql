2c03b53617701f862da8f738b12002cdd705f503
Sidhant-1299 <sidhant.raj.khati@gmail.com>
2026-04-04 01:25:14 -0400

    Updated echo statements for bronze layer data load

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -32,21 +32,28 @@ TRUNCATE TABLE bronze.crm_cust_info;
 
 -- Load in to bronze.crm_prd_info
 TRUNCATE TABLE bronze.crm_prd_info;
+\echo 'copying crm_prd_info...'
 \copy bronze.crm_prd_info FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_crm/prd_info.csv' WITH (FORMAT csv, HEADER true)
 
 -- Load in to bronze.crm_sales_details
 TRUNCATE TABLE bronze.crm_sales_details;
+\echo 'copying crm_sales_details...'
 \copy bronze.crm_sales_details FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_crm/sales_details.csv' WITH (FORMAT csv, HEADER true)
 
 -- Load in to bronze.erp_cust_az12 from file
 TRUNCATE TABLE bronze.erp_cust_az12;
+\echo 'copying erp_cust_az12...'
 \copy bronze.erp_cust_az12 FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_erp/cust_az12.csv' WITH (FORMAT csv, HEADER true)
 
 -- Load in to bronze.erp_loc_a101 from file
 TRUNCATE TABLE bronze.erp_loc_a101;
+\echo 'copying erp_loc_a101...'
 \copy bronze.erp_loc_a101 FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_erp/loc_a101.csv' WITH (FORMAT csv, HEADER true)
 
 
 -- Load in to bronze.erp_px_cat_g1v2 from file
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+\echo 'copying erp_px_cat_g1v2...'
 \copy bronze.erp_px_cat_g1v2 FROM '/Users/sidhant/Desktop/project/sql-data-warehouse-project/datasets/source_erp/px_cat_g1v2.csv' WITH (FORMAT csv, HEADER true)
+
+\echo "Copy completed!"
